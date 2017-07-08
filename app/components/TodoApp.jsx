1cc4d580ce560b2977a67c9b2d8883e6acc3bdc5
Steve <stevecortis@live.com>
2017-07-08 20:08:47 +0200

    Added add todo functionality

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
 var TodoSearch = require('TodoSearch');
+var uuid = require('node-uuid');
 
 var TodoApp = React.createClass({
   getInitialState: function(){
@@ -10,23 +11,31 @@ var TodoApp = React.createClass({
       searchText: '',
       todos: [
         {
-          id: 1,
+          id: uuid(),
           text: 'Walk the dog'
         }, {
-          id: 2,
+          id: uuid(),
           text: 'Clean the yard'
         }, {
-          id: 3,
+          id: uuid(),
           text: 'Clean the yard 3'
         }, {
-          id: 4,
+          id: uuid(),
           text: 'Clean the yard 4'
         }
       ]
     };
   },
   handleAddTodo: function (text){
-    alert('new todo');
+    this.setState({
+      todos: [
+        ...this.state.todos,
+        {
+          id: uuid(),
+          text: text
+        }
+      ]
+    });
   },
   handleSearch: function (showCompleted, searchText){
     this.setState({
